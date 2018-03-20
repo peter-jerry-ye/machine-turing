5c9ca175fe36d841179fd1e68ce1346ceed1a1c9
Corentin Vannier <corentinvannier@protonmail.ch>
2018-03-20 17:28:04 +0100

    Conversion des symboles en entiers
    Création du constructeur
    Modification de lecture() et ecriture()
    Ajout de getPointeur() et getRuban()

--- src/Ruban.java
+++ src/Ruban.java
@@ -1,25 +1,43 @@
 import java.util.*;
+import java.lang.Math;
 
 public class Ruban {
-	private int pointeur = 0;
-	private LinkedList<Character> r;
-	Ruban() {
-		super();
+	private int pointeur;
+	private ArrayList<Integer> ruban;
+	private ArrayList<String> tConversion;
+	Ruban(String texte, String separateur, ArrayList<String> tableau) {
+		String[] donnees = texte.split(separateur);
+		tConversion = tableau;
+		pointeur = Integer.parseInt(donnees[0]); // la premiere donnee est la position initiale du pointeur
+		ruban = new ArrayList<Integer>(donnees.length); // initialisaton de la capacite du ruban
+		for(int i = 1; i < donnees.length; i ++) {
+			 ruban.add(tConversion.indexOf(donnees[i]));
+		}
 	}
 	public void deplacement(int direction) {
 		pointeur += direction;
 	}
-	public void ecriture(char symbole) {
-		if(pointeur < 0) {
-			r.addFirst(symbole);
-			pointeur = 0;
-		} else if(pointeur > r.size()) {
-			r.addLast(symbole);
+	public void ecriture(int symbole) {
+		if(pointeur > ruban.size()) {
+			ruban.add(symbole);
 		} else {
-			r.set(pointeur, symbole);
+			pointeur = Math.max(pointeur, 0);
+			ruban.set(pointeur, symbole);
 		}
 	}
-	public char lecture() {
-		return r.get(pointeur);
+	public int lecture() {
+		return ruban.get(pointeur);
+	}
+	public int getPointeur() {
+		return pointeur;
+	}
+	public ArrayList<String> getRuban() {
+		ArrayList<String> caracteres = new ArrayList<String>(ruban.size());
+		String caractere;
+		for(int i = 0; i < ruban.size(); i ++) {
+			caractere = tConversion.get(ruban.get(i));
+			caracteres.set(i, caractere);
+		}
+		return caracteres;
 	}
 }
