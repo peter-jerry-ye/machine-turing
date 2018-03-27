4b8a0b3165dcaf00a545a3bf2fce2832aa532fac
Corentin Vannier <corentinvannier@protonmail.ch>
2018-03-27 18:01:32 +0200

    Ajout des commentaires à compléter

--- src/Turing_Machine.java
+++ src/Turing_Machine.java
@@ -4,40 +4,48 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 
 public class Turing_Machine {
-	
+
 	private final int LIGNE_ETATS = 0;
 	private final int LIGNE_SYMBOLES = 1;
 	private final int LIGNE_TABLEAU = 2;
-	
+
 	private Ruban ruban;
 	private String rubanOriginal = "";
 	private ArrayList<String> tableauConversionEtats;
 	private ArrayList<String> tableauConversionSymboles;
-	
+
 	private int[][][] tableauAction;
 	private int etat=0;
-	
+	/**
+	 * [Turing_Machine description]
+	 * @param tableauAct         [description]
+	 * @param tableauConvEtat    [description]
+	 * @param tableauConvSymbole [description]
+	 */
 	Turing_Machine(int[][][] tableauAct, ArrayList<String> tableauConvEtat, ArrayList<String> tableauConvSymbole){
 		tableauConversionEtats = tableauConvEtat;
 		tableauConversionSymboles = tableauConvSymbole;
 		tableauAction = tableauAct;
 		}
-		
+	/**
+	 * [Turing_Machine description]
+	 * @param adresse [description]
+	 */
 	Turing_Machine(File adresse){
 		try{
 			List<String> lignes = Files.readAllLines(/*Paths.get(adresse)*/, Charset.defaultCharset());
-			
+
 			String ligneSelectionnee;
 			ligneSelectionnee = lignes.get(LIGNE_ETATS);
 			tableauConversionEtats = new ArrayList<String>(Arrays.asList(ligneSelectionnee.split(" ")));
 			ligneSelectionnee = lignes.get(LIGNE_SYMBOLES);
 			tableauConversionSymboles = new ArrayList<String>(Arrays.asList(ligneSelectionnee.split(" ")));
 			ligneSelectionnee = lignes.get(LIGNE_TABLEAU);
-			
+
 			//Construction du tableau
-			
+
 			tableauAction = new int[tableauConversionEtats.size()][tableauConversionSymboles.size()][3];
-			
+
 			String[] etats = ligneSelectionnee.split("/");
 			String[] symboles;
 			String[] actions;
@@ -53,21 +61,23 @@ public class Turing_Machine {
 				}
 				System.out.println();
 			}
-			
-			
+
+
 		}
 		catch (Exception e) {
          e.printStackTrace();
         }
 		String separateur=" ";
-		
+
 		}
-	
-	
+	/**
+	 * [main description]
+	 * @param args [description]
+	 */
 	public static void main(String[] args){
-	
+
 		int[][][] tableauAction = {
-			{{1,-1,0},{0,-1,1},{2,0,0}}, 
+			{{1,-1,0},{0,-1,1},{2,0,0}},
 			{{0,-1,1},{1,-1,1},{2,-1,2}},
 			{{1,1,3},{0,-1,2},{1,1,3}},
 			{{0,1,3},{1,1,3},{2,1,4}},
@@ -77,11 +87,11 @@ public class Turing_Machine {
 		System.out.println(ruban);
 		int etat=0;
 		int curseur=8;
-		
+
 		while(etat!=-1){
 			int symbole = ruban.get(curseur);
 			int[] actions = tableauAction[etat][symbole];
-		
+
 			ruban.set(curseur,actions[0]);
 			curseur+=actions[1];
 			if(curseur>ruban.size()){break;}
@@ -89,31 +99,36 @@ public class Turing_Machine {
 			System.out.println(ruban + "  " + etat);
 		}
 	}
-	
+	/**
+	 * [next description]
+	 */
 	public void next(){
-		
+
 		int symbole = ruban.lecture();
 		int[] actions = tableauAction[etat][symbole];
-		
+
 		ruban.ecriture(actions[0]);
 		ruban.deplacement(actions[1]);
 		this.etat = actions[2];
-		
+
 	}
-	
+	/**
+	 * [changerRuban description]
+	 * @param  ruban [description]
+	 * @return       [description]
+	 */
 	public String changerRuban(String ruban){
 		ruban = new Ruban(ruban,tableauConversionSymboles);
 		}
-	
+
 /*	public String lireRuban(){
 		return ruban.getRuban();
 		}*/
-	
+
 	//public enregistrerMachine(Path adresse)
-	
+
 	public String lireEtat(){return tableauConversionEtats.get(etat);}
 	
 	public int[][][] lireTableau(){return tableauAction;}
-	
-}
 
+}
